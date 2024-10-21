c12946edb65ecb970c3dd43a006a6b4d94b7d523
Huỳnh Đức Hoàng <huynh.hoang@rivercrane.com.vn>
2024-10-21 16:55:11 +0700

    clean code

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,17 +1,3 @@
 export default function Page() {
-  return <div>
-    <h1 className="h3 mb-1 text-gray-800"></h1>
-    <div className="row">
-      <div className="col-lg-4">
-        <div className="card shadow mb-4">
-          <div className="card-header py-3">
-            <h6 className="m-0 font-weight-bold text-primary">Login</h6>
-          </div>
-          <div className="card-body">
-
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+  return <div/>
 }
