c651a3af41a96486e286b73953b4b4bb65a64f10
Huỳnh Đức Hoàng <huynh.hoang@rivercrane.com.vn>
2024-10-22 13:51:18 +0700

    clean code

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,9 +4,14 @@ import { Authenticator } from '@aws-amplify/ui-react'
 import '@aws-amplify/ui-react/styles.css'
 import { Amplify } from "aws-amplify"
 
-import config from '@/amplifyconfiguration.json'
-
-Amplify.configure(config)
+Amplify.configure({
+    Auth: {
+        Cognito: {
+            userPoolClientId: '2np4u2e23ihfdf3bl1b554ukhl',
+            userPoolId: 'ap-northeast-1_KyLp1nzhQ'
+        }
+    }
+})
 
 export default function Page() {
   return (
