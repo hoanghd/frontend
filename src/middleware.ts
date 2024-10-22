3ab3a009465ff3b0fab1a60aa763d32bd645bf02
Huỳnh Đức Hoàng <huynh.hoang@rivercrane.com.vn>
2024-10-22 16:48:42 +0700

    update

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,10 +3,9 @@ import type { NextRequest } from 'next/server'
 
 export const config = {
     matcher: [
-        '/((?!_next/static|_next/image|favicon.ico|img|css).*)'
+        '/((?!_next/static|_next/image|img|css|login|favicon.ico).*)'
     ]
 }
 export function middleware(request: NextRequest) {
-    console.log(request.url)
     return NextResponse.next()
 }
