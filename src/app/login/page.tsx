91dcd2c9dec970f3877a5477c5eb82af67afe92b
Huỳnh Đức Hoàng <huynh.hoang@rivercrane.com.vn>
2024-10-22 15:44:47 +0700

    update

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,38 +1,3 @@
-'use client'
-
-import { fetchAuthSession } from '@aws-amplify/auth'
-import { Authenticator } from '@aws-amplify/ui-react'
-import '@aws-amplify/ui-react/styles.css'
-import { Amplify } from "aws-amplify"
-
-Amplify.configure({
-    Auth: {
-        Cognito: {
-            userPoolClientId: '2np4u2e23ihfdf3bl1b554ukhl',
-            userPoolId: 'ap-northeast-1_KyLp1nzhQ'
-        }
-    }
-})
-
 export default function Page() {
-    const printAccessTokenAndIdToken = async () => {
-        try {
-            const session = await fetchAuthSession();
-            console.log('Access Token:', session.tokens.accessToken.toString());
-            console.log('ID Token:', session.tokens.idToken.toString());
-        }
-        catch (e) { console.log(e); }
-    }
-
-  return (
-      <Authenticator hideSignUp>
-        {({ signOut, user }) => (
-            <main>
-                <h1>Hello {user.username}</h1>
-                <button onClick={signOut}>Sign out</button><br/>
-                <button onClick={printAccessTokenAndIdToken}>Print Tokens</button>
-            </main>
-        )}
-      </Authenticator>
-  )
+  return <div/>
 }
