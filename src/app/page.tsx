eda904b148b2fea84e1b29c981ba311b961beb3a
Huỳnh Đức Hoàng <huynh.hoang@rivercrane.com.vn>
2024-10-24 10:49:42 +0700

    clean code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,3 @@
-import ResultList from "@/components/Tables/ResultList"
-import FormServer from "@/components/Tables/FormServer"
-
-import {
-  SkeletonTablesResult,
-  SkeletonTablesForm
-} from "@/components/Skeleton"
-
-import { Suspense } from "react"
-
-export default async function Page({searchParams = {}}) {
-  return <div>
-    <h1 className="h3 mb-1 text-gray-800"></h1>
-    <div className="row">
-      <div className="col-sm-12 col-md-12">
-        <div className="card mb-4">
-          <div className="card-header">
-            絞り込み
-          </div>
-          <div className="card-body">
-            <Suspense fallback={<SkeletonTablesForm/>}>
-              <FormServer {...{searchParams}} />
-            </Suspense>
-          </div>
-        </div>
-      </div>
-      <div className="col-sm-12 col-md-12">
-        <div className="table-responsive">
-          <Suspense fallback={<SkeletonTablesResult/>}>
-            <ResultList {...{searchParams}} />
-          </Suspense>
-        </div>
-      </div>
-    </div>
-  </div>
+export default async function Page() {
+  return <div/>
 }
